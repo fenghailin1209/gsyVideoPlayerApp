6126e8d53924c74c54172fabe39e36ac49a8ec78
冯海林 <383272680@qq.com>
2017-11-10 14:26:59 +0800

    1

--- src/main/java/com/example/gsyvideoplayer/RecyclerViewActivity.java
+++ src/main/java/com/example/gsyvideoplayer/RecyclerViewActivity.java
@@ -2,11 +2,11 @@ package com.example.gsyvideoplayer;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 
@@ -15,12 +15,16 @@ import com.example.gsyvideoplayer.delagate.TopicImageItemDelagate;
 import com.example.gsyvideoplayer.delagate.TopicVideoItemDelagate;
 import com.example.gsyvideoplayer.holder.RecyclerItemNormalHolder;
 import com.example.gsyvideoplayer.model.VideoModel;
+import com.example.gsyvideoplayer.utils.AndroidUtil;
 import com.shuyu.gsyvideoplayer.GSYVideoManager;
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
 import com.shuyu.gsyvideoplayer.video.base.GSYVideoPlayer;
 import com.zhy.adapter.recyclerview.MultiItemTypeAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.shuyu.gsyvideoplayer.video.base.GSYVideoView.CURRENT_STATE_PLAYING;
 
 public class RecyclerViewActivity extends Activity {
 
@@ -32,11 +36,13 @@ public class RecyclerViewActivity extends Activity {
     //可是范围内的位置
     int firstVisibleItem, lastVisibleItem;
     private MultiItemTypeAdapter adapter;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+        context = this;
         setContentView(R.layout.activity_recycler_view);
         videoList = (RecyclerView) findViewById(R.id.list_item_recycler);
         mLayoutManager = new LinearLayoutManager(this);
@@ -49,8 +55,6 @@ public class RecyclerViewActivity extends Activity {
         adapter.addItemViewDelegate(new TopicImageItemDelagate());
         adapter.addItemViewDelegate(new TopicVideoItemDelagate(this, TAG));
         videoList.setAdapter(adapter);
-
-        setCommonRecycleViewOnTouchUpOperation(videoList);
 
         setCommonRecycleView(adapter);
     }
@@ -101,8 +105,7 @@ public class RecyclerViewActivity extends Activity {
         dataList.add(videoModel5);
 
         VideoModel videoModel6 = new VideoModel();
-        videoModel6.setType("3");
-        videoModel6.setUrl("http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4");
+        videoModel6.setType("2");
         dataList.add(videoModel6);
 
         VideoModel videoModel7 = new VideoModel();
@@ -110,46 +113,12 @@ public class RecyclerViewActivity extends Activity {
         dataList.add(videoModel7);
 
         VideoModel videoModel8 = new VideoModel();
-        videoModel8.setType("2");
+        videoModel8.setType("3");
+        videoModel8.setUrl("http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4");
         dataList.add(videoModel8);
     }
 
     //TODO=========================== 真正关心 start =======================
-
-    private void setCommonRecycleViewOnTouchUpOperation(final RecyclerView recyclerView) {
-        recyclerView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                int ac = event.getAction();
-                if (ac == MotionEvent.ACTION_UP) {
-                    /*Log.i(TAG, "--->>>firstVisibleItem:" + firstVisibleItem + ",lastVisibleItem:" + lastVisibleItem);
-                    //获取recycleview可见视频item（最上面的一个）
-                    //判断如果这个视频item的最底部离recycleview顶部距离大于视频item的一半则播放，反之关闭。
-                    for (int postion = firstVisibleItem; postion <= lastVisibleItem; postion++) {
-                        VideoModel model = dataList.get(postion);
-                        String type = model.getType();
-                        Log.i(TAG, "--->>>type:" + type);
-                        if (type.equals("3")) {
-//                            View view = recyclerView.getChildAt(postion);
-                            View view = mLayoutManager.getChildAt(postion);
-                            int itemHeight = view.getMeasuredHeight();
-                            if (view != null) {
-                                int[] location = new int[2];
-                                //TODO:测试有点问题
-//                                view.getLocationInWindow(location); //获取在当前窗口内的绝对坐标
-                                view.getLocationOnScreen(location);//获取在整个屏幕内的绝对坐标
-                                Log.i(TAG, "view--->x坐标:" + location[0] + "view--->y坐标:" + (location[1]) + ",itemHeight:" + itemHeight+",postion:"+postion);
-                                break;
-                            }
-                        }
-                    }*/
-
-
-                }
-                return false;
-            }
-        });
-    }
 
     /**
      * TODO:标准设置的写法，基本不用改
@@ -161,6 +130,34 @@ public class RecyclerViewActivity extends Activity {
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int scrollState) {
                 super.onScrollStateChanged(recyclerView, scrollState);
+                for (int postion = firstVisibleItem; postion <= lastVisibleItem; postion++) {
+                        VideoModel model = dataList.get(postion);
+                        String type = model.getType();
+                        if (type.equals("3")) {
+                            //这两个方法都是获取的屏幕内的view
+//                        View view = mLayoutManager.getChildAt(postion);
+//                        View view = recyclerView.getChildAt(postion);
+                            //这两个方法都是获取的adapter的绝对view
+                            View view = mLayoutManager.findViewByPosition(postion);
+                            StandardGSYVideoPlayer standardGSYVideoPlayer = (StandardGSYVideoPlayer) view.findViewById(R.id.video_item_player);
+                            int itemHeight = view.getHeight();
+                            if (view != null) {
+                                int[] location = new int[2];
+                                view.getLocationOnScreen(location);//获取在整个屏幕内的绝对坐标
+                                int locationY = (int) (location[1] - AndroidUtil.getStatusBarHeight(context) - getResources().getDimension(R.dimen.topic_video_height));
+                                int rvHeight = recyclerView.getMeasuredHeight();
+                                int state = standardGSYVideoPlayer.getCurrentState();
+                                if (locationY > -(itemHeight / 2) && locationY < rvHeight - (itemHeight / 2)) {
+                                    if(state != CURRENT_STATE_PLAYING){
+                                        standardGSYVideoPlayer.startPlayLogic();
+                                    }
+                                } else {
+                                    onVideoDestory();
+                                }
+                                break;
+                            }
+                        }
+                    }
             }
 
             @Override
@@ -176,7 +173,6 @@ public class RecyclerViewActivity extends Activity {
                     int position = GSYVideoManager.instance().getPlayPosition();
                     //对应的播放列表TAG
                     String pTag = GSYVideoManager.instance().getPlayTag();
-                    Log.i(TAG, "--->>>sp:" + sp + ",pTag:" + pTag);
                     if (pTag.equals(RecyclerItemNormalHolder.TAG)
                             && (position < firstVisibleItem || position > lastVisibleItem)) {
                         //如果滑出去了上面和下面就是否，和今日头条一样
