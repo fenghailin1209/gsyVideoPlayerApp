74f3e38a92a3837434e80a28ae13161a5065fc7f
冯海林 <383272680@qq.com>
2017-11-10 10:28:23 +0800

    1

--- src/main/java/com/example/gsyvideoplayer/RecyclerViewActivity.java
+++ src/main/java/com/example/gsyvideoplayer/RecyclerViewActivity.java
@@ -1,13 +1,14 @@
 package com.example.gsyvideoplayer;
 
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 
 import com.example.gsyvideoplayer.delagate.TopicCommonItemDelagate;
 import com.example.gsyvideoplayer.delagate.TopicImageItemDelagate;
@@ -21,17 +22,21 @@ import com.zhy.adapter.recyclerview.MultiItemTypeAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerViewActivity extends AppCompatActivity{
+public class RecyclerViewActivity extends Activity {
 
     private static final String TAG = RecyclerViewActivity.class.getSimpleName();
     RecyclerView videoList;
     LinearLayoutManager mLayoutManager;
     List<VideoModel> dataList = new ArrayList<>();
     boolean mFull = false;
+    //可是范围内的位置
+    int firstVisibleItem, lastVisibleItem;
+    private MultiItemTypeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
         setContentView(R.layout.activity_recycler_view);
         videoList = (RecyclerView) findViewById(R.id.list_item_recycler);
         mLayoutManager = new LinearLayoutManager(this);
@@ -39,10 +44,10 @@ public class RecyclerViewActivity extends AppCompatActivity{
 
         initData();
 
-        final MultiItemTypeAdapter adapter = new MultiItemTypeAdapter(this, dataList);
+        adapter = new MultiItemTypeAdapter(this, dataList);
         adapter.addItemViewDelegate(new TopicCommonItemDelagate());
         adapter.addItemViewDelegate(new TopicImageItemDelagate());
-        adapter.addItemViewDelegate(new TopicVideoItemDelagate(this,TAG));
+        adapter.addItemViewDelegate(new TopicVideoItemDelagate(this, TAG));
         videoList.setAdapter(adapter);
 
         setCommonRecycleViewOnTouchUpOperation(videoList);
@@ -111,12 +116,34 @@ public class RecyclerViewActivity extends AppCompatActivity{
 
     //TODO=========================== 真正关心 start =======================
 
-    private void setCommonRecycleViewOnTouchUpOperation(RecyclerView recyclerView) {
+    private void setCommonRecycleViewOnTouchUpOperation(final RecyclerView recyclerView) {
         recyclerView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 int ac = event.getAction();
                 if (ac == MotionEvent.ACTION_UP) {
+                    /*Log.i(TAG, "--->>>firstVisibleItem:" + firstVisibleItem + ",lastVisibleItem:" + lastVisibleItem);
+                    //获取recycleview可见视频item（最上面的一个）
+                    //判断如果这个视频item的最底部离recycleview顶部距离大于视频item的一半则播放，反之关闭。
+                    for (int postion = firstVisibleItem; postion <= lastVisibleItem; postion++) {
+                        VideoModel model = dataList.get(postion);
+                        String type = model.getType();
+                        Log.i(TAG, "--->>>type:" + type);
+                        if (type.equals("3")) {
+//                            View view = recyclerView.getChildAt(postion);
+                            View view = mLayoutManager.getChildAt(postion);
+                            int itemHeight = view.getMeasuredHeight();
+                            if (view != null) {
+                                int[] location = new int[2];
+                                //TODO:测试有点问题
+//                                view.getLocationInWindow(location); //获取在当前窗口内的绝对坐标
+                                view.getLocationOnScreen(location);//获取在整个屏幕内的绝对坐标
+                                Log.i(TAG, "view--->x坐标:" + location[0] + "view--->y坐标:" + (location[1]) + ",itemHeight:" + itemHeight+",postion:"+postion);
+                                break;
+                            }
+                        }
+                    }*/
+
 
                 }
                 return false;
@@ -126,14 +153,15 @@ public class RecyclerViewActivity extends AppCompatActivity{
 
     /**
      * TODO:标准设置的写法，基本不用改
+     *
      * @param adapter
      */
     private void setCommonRecycleView(final MultiItemTypeAdapter adapter) {
         videoList.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            int firstVisibleItem, lastVisibleItem;
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int scrollState) {
-                super.onScrollStateChanged(recyclerView, scrollState);}
+                super.onScrollStateChanged(recyclerView, scrollState);
+            }
 
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
@@ -148,7 +176,7 @@ public class RecyclerViewActivity extends AppCompatActivity{
                     int position = GSYVideoManager.instance().getPlayPosition();
                     //对应的播放列表TAG
                     String pTag = GSYVideoManager.instance().getPlayTag();
-                    Log.i(TAG, "--->>>sp:" + sp+",pTag:"+pTag);
+                    Log.i(TAG, "--->>>sp:" + sp + ",pTag:" + pTag);
                     if (pTag.equals(RecyclerItemNormalHolder.TAG)
                             && (position < firstVisibleItem || position > lastVisibleItem)) {
                         //如果滑出去了上面和下面就是否，和今日头条一样
@@ -171,7 +199,7 @@ public class RecyclerViewActivity extends AppCompatActivity{
     }
 
     private void onVideoDestory() {
-        Log.i(TAG, "--->>>releaseAllVideos" );
+        Log.i(TAG, "--->>>releaseAllVideos");
         GSYVideoPlayer.releaseAllVideos();
     }
     //TODO=========================== 真正关心 end =======================
